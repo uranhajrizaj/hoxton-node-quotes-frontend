c53bf345a8b52ebbfa9074cb470659bf7918369d
Uran Hajrizaj <uranhajrizaj@outlook.com>
2022-08-30 14:30:02 +0200

    radnom quote added

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,11 @@ type Quote={
 
 function App() {
  const[quotes,setQuotes]=useState<Quote[]>([])
+ const[radomquote,setrandomQuote]=useState<Quote>({
+  id:0,
+  title:"",
+  author:""
+ })
 
  useEffect(()=>{
   fetch('http://localhost:4000/quotes')
@@ -18,16 +23,27 @@ function App() {
   .then(quotesFromServer=>setQuotes(quotesFromServer))
  },[])
 
+ useEffect(()=>{
+  fetch('http://localhost:4000/random')
+  .then(resp=>resp.json())
+  .then(quoteFromServer=>setrandomQuote(quoteFromServer))
+ },[])
+
+
   return (
     <div className="App">
       <ul className='quote_list'>
+      <li className='quote random'>
+          Random Quote:
+         <h2> "{radomquote.title}"</h2>  
+         <h3>Author: {radomquote.author}</h3>
+      </li>
+
       {quotes.map(quote=>(
       <li className='quote'>
        <h2> "{quote.title}"</h2>  
        <h3>Author: {quote.author}</h3>
-  
       </li>
-
       )  
       )}
       </ul>
